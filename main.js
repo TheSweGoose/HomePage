7909c70164b1edd1c9291eb9bab8843af9ca5e54
TheSweGoose <105603049+TheSweGoose@users.noreply.github.com>
2022-08-28 12:59:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const setTime = () => {
+  const day = document.querySelector(".day");
+  const month = document.querySelector(".month");
+  const dayOfTheMonth = document.querySelector(".day-of-the-month");
+
+  const hours = document.querySelector(".hours");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
+
+  const currentDate = new Date();
+
+  day.innerText = week[currentDate.getDay()];
+  month.innerText = months[currentDate.getMonth()];
+  dayOfTheMonth.innerText = currentDate.getDate();
+
+  hours.innerText = currentDate.getHours().toString().padStart(2, "0");
+  minutes.innerText = currentDate.getMinutes().toString().padStart(2, "0");
+  seconds.innerText = currentDate.getSeconds().toString().padStart(2, "0");
+};
+
+setInterval(setTime, 1000);
